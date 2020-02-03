9ba97f11cfa975a1d2f4348d66e05b4bca70158f
manav <manavmaini11@gmail.com>
2020-02-04 00:26:38 +0530

    layouts managed

--- app/src/main/java/com/example/air_pollution/loginActivity.java
+++ app/src/main/java/com/example/air_pollution/loginActivity.java
@@ -1,21 +1,92 @@
 package com.example.air_pollution;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class loginActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+    private String url = " ";
+    private ProgressBar progressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.user_name);
+        password = findViewById(R.id.password);
+        progressBar = findViewById(R.id.spin_kit);
+
+
     }
 
     public void onClick_login(View view) {
-        Intent intent = new Intent(this,MainActivity.class);
-        startActivity(intent);
+        progressBar.setVisibility(View.VISIBLE);
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        progressBar.setVisibility(View.GONE);
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressBar.setVisibility(View.GONE);
+
+            }
+        }) {
+
+            @Override
+            protected Map getParams() {
+                Map params = new HashMap();
+                params.put("user_name",username.getText().toString());
+                params.put("password",password.getText().toString());
+                return params;
+            }
+
+        };
+
+        jsonObjectRequest.setRetryPolicy(new RetryPolicy() {
+            @Override
+            public int getCurrentTimeout() {
+                return 5000;
+            }
+
+            @Override
+            public int getCurrentRetryCount() {
+                return 5000;
+            }
+
+            @Override
+            public void retry(VolleyError error) throws VolleyError {
+
+            }
+        });
+
+        requestQueue.add(jsonObjectRequest);
     }
+
 }
